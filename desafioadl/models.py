b7d68a56b37bb26e89333619372b06cfa4ed3bce
Guillermo Carrasco Faus <gcarrascofaus@gmail.com>
2024-05-17 15:15:26 -0400

    desafio2 terminado

--- desafioadl/models.py
+++ desafioadl/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    is_active = models.BooleanField(default=False)
+    creado_en = models.DataTimeField(auto_now_add=True)
+    actualizado_en = models.DataTimeField(auto_now=True)
 
-class Persona(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    correo = models.EmailField(max_length=50)
+    def __str__(self):
+        return self.nombre
+    
+class SubTarea(models.Model):
+    tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    is_active = models.BooleanField(default=False)
+    creado_en = models.DataTimeField(auto_now_add=True)
+    actualizado_en = models.DataTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    @property
+    def main_task(self):
+        return self.tarea.nombre
--- desafioadl/services.py
+++ desafioadl/services.py
@@ -1,24 +1,51 @@
-from .models import Persona
+from .models import Tarea, SubTarea
 
-def crear_tarea(descripcion):
-    tarea = Tarea.objects.create(descripcion=descripcion)
-    imp()
+def recupera_tareas_y_subtareas():
+    tareas = Tarea.objects.all()
+    tareas_list = []
+    for tarea in tareas:
+        subtareas = SubTarea.objects.filter(tarea=tarea)
+        tareas_list.append({
+            "tarea": tarea,
+            "subtarea": subtareas
+        })
+    return tareas_list
 
-def crear_subtarea(descripcion, tarea):
-    SubTarea.objects.create(descripcion=descripcion, tarea=tarea)
-    imp()
+def crear_nueva_tarea(nombre, descripcion):
+    tarea = Tarea.objects.create(
+        nombre = nombre,
+        descripcion = descripcion
+    )
+
+    return tarea
+
+def crear_nueva_subtarea(tarea, nombre, descripcion):
+    if isinstance(tarea, int):
+        tarea = Tarea.objects.get(id=tarea)
+    subtarea = SubTarea.objects.create(
+        tarea = tarea,
+        nombre = nombre,
+        descripcion = descripcion
+    )
+    return subtarea
 
 def eliminar_tarea(tarea_id):
-    tarea = Tarea.objects.filter(id=tarea_id).update(eliminada=True)
-    Subtarea.objects.filter(tarea_id=tarea_id).update(eliminada=True)
-    imp()
+    tarea = Tarea.objects.get(id=tarea_id)
+    tarea.delete()
 
-def elimina_subtarea(subtarea_id):
-    subtarea = Subtarea.objects.filter(id=subtarea_id).update(eliminada=True)
-    imp()
+def eliminar_subtarea(subtarea_id):
+    subtarea = SubTarea.objects.get(id=subtarea_id)
+    subtarea.delete()
 
-def imp():
-    print('Tarea: ', Tarea.objects.all())
-    print('Subtarea: ', Subtarea.objects.all())
-    print('Persona: ', Persona.objects.all())
-    print('----------------------')
+def imprimir_en_pantalla():
+    tareas = recupera_tareas_y_subtareas()
+    tarea_numero = 0
+    subtarea_numero = 0
+    print('Tareas y subtareas:')
+    for tarea in tareas:
+        tarea_numero += 1
+        print(f'[{tarea_numero}] {tarea["tarea"].name}')
+        for subtarea in tarea["subtarea"]:
+            subtarea_numero += 1
+            print(f'....[{subtarea_numero}]{subtarea.name}')
+    return tareas
